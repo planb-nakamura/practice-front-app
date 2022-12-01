38042370dbbbc53b01e14b8a3b5888a99763f710
Ren Nakamura <ren.nakamura@plan-b.co.jp>
2022-12-01 13:57:48 +0900

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[posts, setPosts] = useState([]);
+  //データ一覧
+  axios
+  .get('localhost:18080/v1/note')            
+  .then(response => {
+      setPosts(response.data);
+      console.log(response.data);
+  })                              
+  .catch(() => {
+      console.log('通信に失敗しました');
+  });     
+  return(
+    <></>
+  )
+
+  //データ送信
+  
+
 }
 
 export default App;
